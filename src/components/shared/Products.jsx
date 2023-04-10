9d6bbb7767e0b38f7fc6a3d10fe92433c29faf57
hmdns26 <hmd996hh@gmail.com>
2023-04-10 13:24:40 +0430

    add helper functions

--- src/components/shared/Products.jsx
+++ src/components/shared/Products.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { shorten } from '../../helpers/functions';
 const Products = ({productData}) => {
     return (
         <div>
             <img src={productData.image} alt="mahsul" style={{width:'200px'}}/>
-            
+            <h3>{shorten(productData.title)}</h3>
             <p>{productData.price}</p>
             <button>add card</button>
        </div>
